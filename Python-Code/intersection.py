afb73d5085f4c61095e267706a52f87e99d68d99
Trece Wicklander-Bryant <trecewicklander@hotmail.com>
2018-05-18 19:01:27 -0700

    More variables and functions added to intersection.py

--- Python-Code/intersection.py
+++ Python-Code/intersection.py
@@ -1,11 +1,70 @@
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
 
-class Intersection(object):
-	def __init__(self, city_name):
-		self.id = city_name
-		self.distance = sys.maxsize
-		self.edges = []
-		self.visted = False
+class intesection(object):
+	def __init__(self, name, x, y):
+		self.id = name							#int - Unique identifier for this node
+		self.x = x 								#int - X coordinate on the map
+		self.y = y 								#int - Y coordinate on the map
+		self.queue = []							#Queue - Queue of Cars at this location
+		self.q_size = 0							#int - Current number of elements in the queue
+		self.edge_list = [] 					#List - List of connected edges
+		self.cap = len(self.edge_list)			#int - Allowed size of queue
+		self.ts = 2								#int - Number of time steps to pass through node
+		self.visted = False						#bool - Used for dijkstra's shortest path
 
-	def add_road(self, road):
+	def add_node(self, road):
 		self.edges.append(edge)
+
+	def add(self, car):
+		self.queue.append(car)					#Place the car onto the queue
+		self.q_size += 1						#Properly track queue size
+		car.ts_on_obj = 0						#Reset the time on object for the car
+
+	def remove(self):
+		self.queue.pop()						#Remove the front car from the queue
+		self.q_size -= 1						#Properly track queue size
+
+	def run(self):
+		keep_moving = True						#Flag that determines if you should keep looping
+		curr_car = 0							#Index of the current item you're looking at in the queue
+
+		for car in self.queue:					#Go through each item in the queue if you can and move accordingly
+			if(not keep_moving):				#If the one of the front cars couldn't go you need to break
+				break
+
+			if(car.ts_on_obj == self.ts):
+				moved = car.modified_dijkstra()	#Returns True or False depending if the car was able to move or not
+
+				if(moved):
+					self.remove()				#Pop the car off the queue since it moved to an edge
+
+			else:								#If it was not time for the car to move yet just
+				car.ts_on_obj += 1				#increment how long it's been at the node for
+
+			keep_moving = False
+			print(car)
+
+		def modified_dijkstra(self, car):
+			decide = np.random.randint(0,2).astype(bool)
+
+			if(decide):
+				print("You've been give your next node")
+				return True
+			else:
+				print("You can't move")
+				return False
+
+test = intesection("a", 1, 2)
+
+node = np.load("node.npy")
+edge = np.load("edge.npy")
+
+#print(edge)
+#print(node)
+
+x_vals = node[:, 1].astype(np.float64)
+y_vals = node[:, 2].astype(np.float64)
+
+plt.plot(x_vals, y_vals, 'o')
+#plt.show()
--- Python-Code/map.py
+++ Python-Code/map.py
@@ -3,14 +3,32 @@ import networkx as nx
 import geopandas as gpd 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 #Latitude and logitude coordinates of downtown Seattle 
 place_point = (47.608013, -122.335167)
-graph = ox.graph_from_point(place_point, network_type='drive')
-#fig, ax = ox.plot_graph(graph)
+print("Making graph")
+graph = ox.graph_from_point(place_point, network_type='drive',distance=100)
+fig, ax = ox.plot_graph(graph)
 
-nodes, edges = ox.graph_to_gdfs(graph, nodes=True, edges=True)
-print(nodes.columns)
-print(nodes.geometry)
-print(edges.columns)
-print(edges.maxspeed, edges.lanes, edges.length, edges.u, edges.v, edges.geometry)
+#nodes, edges = ox.graph_to_gdfs(graph, nodes=True, edges=True)
+#node, edge = ox.graph_to_gdfs(graph, nodes=True, edges=True)
+#node_data = node.values[:,[2,3]]
+#print(node_data)
+#print(node.columns)
+#print(nodes.geometry)
+#print(edges.columns)
+#print(edges.maxspeed, edges.lanes, edges.length, edges.u, edges.v, edges.geometry)
+edge = np.load("C:/Users/trece/Downloads/edge.npy")
+print(np.shape(edge))
+node = np.load("C:/Users/trece/Downloads/node.npy")
+print(node)
+x_vals = node[:, 1].astype(np.float64)
+y_vals = node[:, 2].astype(np.float64)
+print("Plotting")
+plt.plot(x_vals, y_vals, 'o')
+print("Showing")
+plt.ioff()
+plt.show()
+plt.close()
+print("Should close")
