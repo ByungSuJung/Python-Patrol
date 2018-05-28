9be1d40b673c6e25377e242be1e0dd18161a59b2
samqiancheng <samqian2@gmail.com>
2018-05-27 21:07:16 -0700

    Sam

--- Python-Code/road.py
+++ Python-Code/road.py
@@ -1,4 +1,4 @@
-
+import queue as q
 
 class Road(object):
     def __init__(self, id, start, destination, max_speed, num_lanes, length):
@@ -14,7 +14,7 @@ class Road(object):
         self.length = length            # int
         self.calculate_time_steps()     # int (floor)
         self.calculate_capacity()       # int 
-        self.queue = []
+        self.queue = q.Queue(maxsize=self.capacity)
         self.q_size = 0
     def __hash__(self):
         return hash(str(self))
@@ -29,17 +29,14 @@ class Road(object):
         (int) (((self.length / self.max_speed) * 3600) / self.ONE_TIME_STEP) 
     
     def add(self, car):
-        self.queue.append(car)
+        self.queue.put_nowait(car)
         self.q_size += 1
         car.ts_on_current_position = 0 
 
     def remove(self): 
-        self.queue.pop(0)
+        self.queue.get_nowait()
         self.q_size -= 1
-
-    def run(self): 
-        for car in self.queue: 
-            car.move()
+        car.ts_on_current_position = 0 
 
 if __name__ == '__main__': 
     myRoad = Road(0, 153426, 141414, 40, 2, 2111)
