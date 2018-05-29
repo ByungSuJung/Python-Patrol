da6c6a142cbabe2b1305f39f632a6e3f4d0b97ef
Cole Amaratunge <camaratunge@gmail.com>
2018-05-28 21:23:33 -0700

    Fixing directories

--- Python-Code/driver.py
+++ Python-Code/driver.py
@@ -1,52 +1,14 @@
-import osmnx as ox
-import networkx as nx 
-import numpy as np
-from t_node import TNode 
-from road import Road
+from map import Map 
+import visualize
 
-class Driver:
-	def __init__(self, center_lat=47.608013, center_long=-122.335167, dist=150):
-		center_pt = (center_lat, center_long)
-		graph = ox.graph_from_point(center_pt, network_type='drive', distance=dist)
+m = Map()
+visualize.drawMap(m.nod
 
-		t_node_dict = 
-		for n in graph.nodes(data=True):
-			n = n[1]
-			node_to_insert = TNode(n['osmid'],n['x'],n['y'])
-			node_dict[n['osmid']] = node_to_insert
-		
+"""
+Start the clock
+"""
+while car_list:
 
-		self.node_map = node_dict
-		
-		edge_list = []
-		id = 0
-		for e in g.edges(data=True):
-			f = e[2]
-			print(f)
-			start = e[0]
-			destination = e[1]
-
-			if 'maxspeed' in f.keys():
-				max_speed =  int(f['maxspeed'][0].split(" ")[0]) 
-			else:
-				print("check if/else is working, maxspeed")
-				max_speed = 25 
-
-			if 'lanes' in f.keys():
-				num_lanes = int(f['lanes'][0]) 
-			else:
-				print("check if/else is working, lanes")
-				num_lanes = 1 
-
-			length = f['length'] 
-
-			edge_to_insert = Road(id, start, destination, max_speed, num_lanes, length)
-		
-
-			edge_list.append(edge_to_insert)
-			id+=1
-
-		self.edge_map = edge_list
 
 
 
--- Python-Code/map.py
+++ Python-Code/map.py
@@ -1,34 +1,77 @@
-import osmnx as ox 
-import networkx as nx
-import geopandas as gpd 
-import matplotlib.pyplot as plt
-import pandas as pd
+
+import osmnx as ox
+import networkx as nx 
 import numpy as np
+import random as rn 
+from intersection import Intersection
+from road import Road 
+from car import Car
 
-#Latitude and logitude coordinates of downtown Seattle 
-place_point = (47.608013, -122.335167)
-print("Making graph")
-graph = ox.graph_from_point(place_point, network_type='drive',distance=100)
-fig, ax = ox.plot_graph(graph)
+class Map:
+	def __init__(self, center_lat=47.608013, center_long=-122.335167, \
+		dist=500, num_cars=10):
+		center_pt = (center_lat, center_long)
+		G = ox.graph_from_point(center_pt, network_type='drive', distance=dist)
 
-#nodes, edges = ox.graph_to_gdfs(graph, nodes=True, edges=True)
-#node, edge = ox.graph_to_gdfs(graph, nodes=True, edges=True)
-#node_data = node.values[:,[2,3]]
-#print(node_data)
-#print(node.columns)
-#print(nodes.geometry)
-#print(edges.columns)
-#print(edges.maxspeed, edges.lanes, edges.length, edges.u, edges.v, edges.geometry)
-edge = np.load("C:/Users/trece/Downloads/edge.npy")
-print(np.shape(edge))
-node = np.load("C:/Users/trece/Downloads/node.npy")
-print(node)
-x_vals = node[:, 1].astype(np.float64)
-y_vals = node[:, 2].astype(np.float64)
-print("Plotting")
-plt.plot(x_vals, y_vals, 'o')
-print("Showing")
-plt.ioff()
-plt.show()
-plt.close()
-print("Should close")
+		edge_dict = {}
+		id = 0
+		for e in G.edges(data=True):
+			start = e[0]
+			destination = e[1]
+			tmp = e[2]['maxspeed']
+			max_speed = tmp.split(" ")[0]
+			num_lanes = e[2]['lanes'] 
+			length = e[2]['length'] 
+			#edge_to_insert = Road(id, start, destination, max_speed, num_lanes, length)
+			#edge_list.append = edge_to_insert
+			id+=1
+		self.edge_map = edge_dict
+
+		node_dict = {}
+		for n in G.nodes(data=True):
+			name = n[1]['osmid']
+			x = n[1]['x']
+			y = n[1]['y']
+			outgoing_edges = [] #edges leaving this current node
+			incoming_edges = [] #edges arriving to this current node
+			incoming_lanes = 0  
+			accsessible_nodes = [] #nodes that can be accessesed from this node
+			 
+			for e in edge_list:
+				if e.u == name:
+					outgoing_edges.append(e.id)
+					accsessible_nodes.append(e.v)
+				if e.v == name:
+					incoming_edges.append(e.id)
+					incoming_lanes += e.num_lanes
+			#node_to_insert = TNode()
+			#node_dict[n['osmid']] = node_to_insert
+		self.node_map = node_dict
+
+		start = rn.choice(node_dict.keys())
+		destination = rn.choice(node_dict.keys())
+		path = nx.dijkstra_path(G,start,destination)
+		car_list = []
+		for c in range(num_cars):
+			"""
+			if rand node is full:
+			Option A 
+			init car on a diff node 
+			option B
+			init car on on next t-frame
+			option C
+			init car on nearest road obj where start node
+			would be either null or the closest edge
+			choosing option B 
+			"""
+			#start = rn.choice(node_dict.keys())
+			#destination = rn.choice(node_dict.keys())
+			"""
+			if destination == start:
+				then what???
+			"""
+			#path = nx.dijkstra_path(G,start,destination)
+			car_list.append(Car(start, destination, path))
+
+		self.car_map = car_list
+
